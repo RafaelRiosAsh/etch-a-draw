154bd05655ca537ebcecc5d2ea94d20055b627f0
RafaelRios <rafaeleduardoriosgarcia@gmail.com>
2020-01-20 22:42:02 -0600

    added event so it works with phones

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const body = document.querySelector('.main_div');
 
 let y;
-for (y = 0; y < 100; y++){
-    const lineDiv = document.createElement('div');
-    lineDiv.setAttribute('class', "lineDiv");
-    body.appendChild(lineDiv);
-    let x;
-    for (x = 0; x < 100; x++){
-        const div = document.createElement('div');
-        div.addEventListener('mouseenter', function (e) {
-                e.target.style.background = 'red';
-        });
+for (x = 0; x < 10000; x++){
+    const div = document.createElement('div');
+    div.addEventListener('mouseover', function (e) {
+        e.target.style.background = 'red';
+    });
     
-        div.setAttribute('class', "unChecked");
-        lineDiv.appendChild(div);
-    }
+    div.addEventListener('touchmove', function (e) {
+        e.target.style.background = 'red';
+    });
+
+    div.setAttribute('class', "unChecked");
+    body.appendChild(div);
 }
